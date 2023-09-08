7cb876c8a2a453ab65581b2d25cd6836450c6039
umarhamzy <138566943+umarhamzy@users.noreply.github.com>
2023-09-08 16:01:28 +0530

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+function Message() {
+  // JSX: JavaScript XML
+  const name = "Umar";
+  if (name) {
+    return <h1>Hello, Umar!</h1>;
+  }
+  return <h1>Hello world!</h1>;
+}
+
+export default Message;
