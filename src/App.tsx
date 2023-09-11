336f9cadec13dafd5fc425427e3594725ba22a24
umarhamzy <138566943+umarhamzy@users.noreply.github.com>
2023-09-11 09:55:06 +0530

    Make reusable Alert Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
+console.log("Hello world!");
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span className="text-success">world!</span>
+      </Alert>
     </div>
   );
 }
